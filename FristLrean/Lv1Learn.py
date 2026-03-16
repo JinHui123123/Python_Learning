971567f7c07fa4054797f5691c9203867fde7c2e
JusTH <164820528+JinHui123123@users.noreply.github.com>
2026-03-16 14:41:06 +0800

    查看在另一台笔记本中的提交git

--- FristLrean/Lv1Learn.py
+++ FristLrean/Lv1Learn.py
@@ -2,13 +2,16 @@
 a = -6
 b = -6
 
-print(a is b)
+# print(a is b)
 
 a = -5
 b = -5
-print(a is b)
+# print(a is b)
 
 a = 1000
 b = 1000
-print(a is b)
+# print(a is b)
+
+c,d = 1,2.2
+print(c+d)
 
