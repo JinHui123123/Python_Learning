f97e3b7d4335dc095eddebc4204da32470f57362
JusTH <164820528+JinHui123123@users.noreply.github.com>
2026-04-27 15:21:24 +0800

    desk上传
    多余文件

---
+++ FristLrean/Lv1Learn.py
@@ -0,0 +1,14 @@
+#同一运算符
+a = -6
+b = -6
+
+print(a is b)
+
+a = -5
+b = -5
+print(a is b)
+
+a = 1000
+b = 1000
+print(a is b)
+
---
+++ FristLrean/Lv2Learn.py
@@ -0,0 +1,27 @@
+#字符串
+a = "I' am a string"
+
+b = ''
+
+resume = ('我是 "超级"  '
+          '大大大  '
+          '“大佬” '
+          '吗？')
+
+#转义字符
+
+a = 'I \nlove\nu' #换行
+
+b = 'I \'am a student' #单引号
+
+a = 'love'*3 #输出三次
+
+## 拼接
+# print('aa',end='*')
+# print('bb',end='*')
+# print('cc')
+
+#输入
+uname  = input("请输入你的名字:")
+
+print(uname)
