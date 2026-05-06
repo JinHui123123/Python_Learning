7c6c67a5e8663dde9dfeed12a2e31f85b8321c0e
JusTH <164820528+JinHui123123@users.noreply.github.com>
2026-05-06 21:43:25 +0800

    楚怡学习
    视频9学习完毕

---
+++ CYPython/day02/01-百度产品网页.py
@@ -0,0 +1,22 @@
+from http.client import responses
+from uu import encode
+
+import requests
+
+
+
+# 声明爬取的网站地址 <Respons [200]> 表示获取数据成功
+url = "https://www.baidu.com/more/"
+
+# 对地址发起请求
+response = requests.get(url)
+# 一般情况下使用 text
+# html = response.text
+
+#对文件进行解码  有的网站编码时gbk，gbk-2321
+html = response.content.decode('utf-8')
+
+print(html)
+
+with open("./html/baidu.html","w",encoding="utf-8") as f:
+    f.write(html)
